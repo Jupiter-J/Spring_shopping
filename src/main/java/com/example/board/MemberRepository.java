0b47d7bc6e03d264d41aa46db60a48a13c105806
Jupiter-J <namju2912@gmail.com>
2022-04-07 23:53:47 +0900

    Initial commit

---
+++ src/main/java/com/example/board/MemberRepository.java
@@ -0,0 +1,25 @@
+package com.example.board;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext  //스프링부트가 EntityManger를 주입한다
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+
+
+}
